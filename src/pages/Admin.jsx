83ab047a2ef830646d822d3e890edb11d42100df
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-04-22 11:03:57 +0200

    delete and add tab layout

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,7 @@
 import InputField from "../components/InputField";
 // NPM package
 import { useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 // Project files
 import form from "../data/dishForm.json";
@@ -13,32 +14,66 @@ export default function Admin() {
 	const [description, setDescription] = useState("");
 	const [price, setPrice] = useState("");
 	return (
-		<div>
-			<form className="add-form container">
-				<h2>Add a new dish</h2>
-				<InputField setup={form.category} state={[category, setCategory]} />
-				<InputField setup={form.name} state={[name, setName]} />
-				<InputField setup={form.description} state={[description, setDescription]} />
-				<InputField setup={form.price} state={[price, setPrice]} />
-				<div className="upload-img">
-					<InputField setup={form.imgURL} state={[imgURL, setImgURL]} />
-					<button className="btn-upload">
-						<label class="custom-file-upload" for="file-upload">
-							Or upload image:
-							<img src="/images/upload-to-cloud.png" alt="upload" />
-						</label>
-						<input
-							id="file-upload"
-							className="file-upload"
-							type="file"
-							accept="image/png, image/jpg"
-						/>
-					</button>
-				</div>
+		<div className="admin container">
+			<Tabs className="tabs" defaultIndex={1}>
+				<TabList className="tab-list">
+					<Tab className="tab-add">Add a new dish</Tab>
+					<Tab className="tab-delete">Delete a dish</Tab>
+				</TabList>
 
-				<button className="form-button">Submit</button>
-				<button className="form-button">Reset</button>
-			</form>
+				<TabPanel>
+					<form className="add-form container">
+						<h3>Add a new dish</h3>
+						<InputField setup={form.category} state={[category, setCategory]} />
+						<InputField setup={form.name} state={[name, setName]} />
+						<InputField
+							setup={form.description}
+							state={[description, setDescription]}
+						/>
+						<InputField setup={form.price} state={[price, setPrice]} />
+						<div className="upload-img">
+							<InputField setup={form.imgURL} state={[imgURL, setImgURL]} />
+							<button className="btn-upload">
+								<label className="custom-file-upload" htmlFor="file-upload">
+									Or upload image:
+									<img src="/images/upload-to-cloud.png" alt="upload" />
+								</label>
+								<input
+									id="file-upload"
+									className="file-upload"
+									type="file"
+									accept="image/png, image/jpg"
+								/>
+							</button>
+						</div>
+
+						<button className="form-button">Submit</button>
+						<button className="form-button">Reset</button>
+					</form>
+				</TabPanel>
+				<TabPanel>
+					<form className="add-form container">
+						<h3>Delete a new dish</h3>
+						<select className="form-select">
+							<option selected disabled>
+								Category
+							</option>
+							<option value="1">Beef</option>
+							<option value="2">Pork</option>
+							<option value="3">Chicken</option>
+							<option value="3">Vegetables</option>
+						</select>
+						<select className="form-select">
+							<option selected>Name</option>
+							<option value="1">dish 1</option>
+							<option value="2">dish 2</option>
+							<option value="3">dish 3</option>
+							<option value="3">dish 4 </option>
+						</select>
+						<button className="delete">Delete</button>
+					</form>
+				</TabPanel>
+			</Tabs>
 		</div>
 	);
 }
