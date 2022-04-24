667365cf471a58ecc33dac9713d96bca4e611f5c
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-04-24 13:49:12 +0200

    404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,11 @@
+import "../styles/NotFound.css";
+
 export default function NotFound() {
-	return <div>NotFound</div>;
+	return (
+		<div className="not-found container">
+			<h1>A...www,Don't cry</h1>
+			<h2>It's just a 404 error</h2>
+			<img src="images/404.png" alt="" />
+		</div>
+	);
 }
